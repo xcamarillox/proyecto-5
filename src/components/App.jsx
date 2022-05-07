b7c497079d1fcf1343ddb4f6fbf041450504f939
CARLOS CAMARILLO <carlos.adrian.camarillo@gmail.com>
2022-05-07 18:56:27 -0500

    Search for movie feature ready.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,43 @@
 import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { Input } from 'antd';
+import { Input, message } from 'antd';
 
 import Navbar from "./Navbar";
 import MoviesList from "./MoviesList";
+import { ACTIONS_LIST, getAPIdata } from '../scripts/api-helpers';
 
 const App =  () => {
-    let [moviesArr, setMoviesArr] = useState([
-    {
-        "adult": false,
-        "backdrop_path": "/nNmJRkg8wWnRmzQDe2FwKbPIsJV.jpg",
-        "genre_ids": [
-          878,
-          28,
-          12
-        ],
-        "id": 24428,
-        "original_language": "en",
-        "original_title": "The Avengers",
-        "overview": "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!",
-        "popularity": 242.869,
-        "poster_path": "/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg",
-        "release_date": "2012-04-25",
-        "title": "The Avengers",
-        "video": false,
-        "vote_average": 7.7,
-        "vote_count": 26838
-      },
-      {
-        "adult": false,
-        "backdrop_path": "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
-        "genre_ids": [
-          12,
-          878,
-          28
-        ],
-        "id": 299534,
-        "original_language": "en",
-        "original_title": "Avengers: Endgame",
-        "overview": "After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.",
-        "popularity": 238.218,
-        "poster_path": "/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
-        "release_date": "2019-04-24",
-        "title": "Avengers: Endgame",
-        "video": false,
-        "vote_average": 8.3,
-        "vote_count": 20939
-      },  
-    ]);
-
-    const [inputValue, setInputValue] = useState('')
+    const [moviesArr, setMoviesArr] = useState();
+    const [inputValue, setInputValue] = useState()
     const handleChange = (e) => setInputValue(e.target.value)
+    
+    const handleMovieSearch = async (searchedMovie) => {
+        if (searchedMovie.trim().length != 0){
+            try{
+                const response = await getAPIdata({
+                    type: ACTIONS_LIST.SEARCH_FOR_MOVIES,
+                    searchedMovie
+                })
+                if (!(response && response.success!==false)) throw new Error('Error del servidor');
+                if (response.results.length == 0) {
+                    message.error(`No se tuvieron resultados con ${searchedMovie.trim()}`);
+                    return;
+                }
+                setMoviesArr(response.results);
+                console.log(response);
+            }catch(error){
+                message.error(error.message);
+            }
+        }
+    }
+
     const inputSearchProps = {
-        placeholder:"Buscar por nómbre acá...",
+        placeholder:"Busca tu película...",
         allowClear: true,
         enterButton:"Buscar",
         size:"large",
+        onSearch: handleMovieSearch,
         onChange:handleChange,
         value: inputValue
     }
--- src/scripts/api-helpers.js
+++ src/scripts/api-helpers.js
@@ -1,7 +1,8 @@
 const API_KEY_MOVIEDB = process.env.API_KEY_MOVIEDB;
 
 export const ACTIONS_LIST = {
-    SEARCH_FOR_MOVIES: 'SEARCH_FOR_MOVIES'
+    SEARCH_FOR_MOVIES: 'SEARCH_FOR_MOVIES',
+    SEARCH_TRENDS: 'SEARCH_TRENDS'
 }
 
 export const getImgEndpoint = (img_path) => 'https://image.tmdb.org/t/p/w500' + img_path;
@@ -16,6 +17,9 @@ export const getAPIdata = async(action) => {
     switch (action.type) {
         case ACTIONS_LIST.SEARCH_FOR_MOVIES:
             endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY_MOVIEDB}&language=en-US&page=1&include_adult=false&query=${action.searchedMovie}`
+            return await fetchFunctionGET(endpoint);
+        case ACTIONS_LIST.SEARCH_TRENDS:
+            endpoint = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY_MOVIEDB}`
             return await fetchFunctionGET(endpoint);
         default:
             throw new Error('Nothing to fetch: no match for action.type');
